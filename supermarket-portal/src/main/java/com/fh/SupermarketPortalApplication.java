f2017f304ef8e79c6a0667f32c2e117c7e991322
zhangxuhao <212145091@qq.com>
2020-02-09 18:48:17 +0800

    第4次

---
+++ supermarket-portal/src/main/java/com/fh/SupermarketPortalApplication.java
@@ -0,0 +1,13 @@
+package com.fh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermarketPortalApplication.class, args);
+    }
+
+}
